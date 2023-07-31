0395d74f6ec8b2296083d37f9a6f572d5edf790b
Ivan Shalaiev <ivanshalaev1990@gmail.com>
2023-07-31 21:45:03 +0200

    Add module four task 9

---
+++ JavaCore15/src/ua/com/goit/javacore/modulefour/task9/CheapStocks.java
@@ -0,0 +1,22 @@
+package ua.com.goit.javacore.modulefour.task9;
+
+import java.util.Arrays;
+
+public class CheapStocks {
+    public String getCheapStocks(String[] stocks){
+        StringBuilder sb = new StringBuilder();
+        for (String str: stocks) {
+            String[] separated = str.split(" ");
+            if (Integer.parseInt(separated[1]) < 200){
+                sb.append(separated[0]).append(" ");
+            }
+        }
+
+        return String.valueOf(sb).strip();
+    }
+
+    public static void main(String[] args) {
+        CheapStocks cs = new CheapStocks();
+        System.out.println(cs.getCheapStocks(new String[]{"gun 500", "firebow 70", "pixboom 200", "xi 118", "Ha 138"}));
+    }
+}
