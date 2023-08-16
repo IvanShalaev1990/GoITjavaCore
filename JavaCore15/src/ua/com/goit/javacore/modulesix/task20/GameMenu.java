212d67ae95d1b4d0c54f3a017a8672f3a8ce7aab
Ivan Shalaiev <ivanshalaev1990@gmail.com>
2023-08-16 18:10:01 +0200

    Add module six task 20

---
+++ JavaCore15/src/ua/com/goit/javacore/modulesix/task20/GameMenu.java
@@ -0,0 +1,28 @@
+package ua.com.goit.javacore.modulesix.task20;
+
+public class GameMenu {
+    public void act(MenuItem item){
+        System.out.println(item.getAction());
+    }
+    public abstract static class MenuItem {
+        public abstract String getAction();
+    }
+    public static class Start extends MenuItem {
+        @Override
+        public String getAction(){
+            return "start";
+        }
+    }
+    public static class Options extends MenuItem {
+        @Override
+        public String getAction(){
+            return "options";
+        }
+    }
+    public static class Exit extends MenuItem {
+        @Override
+        public String getAction(){
+            return "exit";
+        }
+    }
+}
---
+++ JavaCore15/src/ua/com/goit/javacore/modulesix/task20/GameMenuTest.java
@@ -0,0 +1,16 @@
+package ua.com.goit.javacore.modulesix.task20;
+
+public class GameMenuTest {
+    public static void main(String[] args) {
+        GameMenu menu = new GameMenu();
+
+        //options
+        menu.act(new GameMenu.Options());
+
+        //start
+        menu.act(new GameMenu.Start());
+
+        //exit
+        menu.act(new GameMenu.Exit());
+    }
+}
